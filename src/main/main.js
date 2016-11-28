751594684d4442d3b5c1f4c51eec0e651e67b7b4
schmooblidon <will_beats@hotmail.co.uk>
2016-11-28 15:20:01 +0000

    fixed typo on dom elements

--- src/main/main.js
+++ src/main/main.js
@@ -1185,8 +1185,8 @@ window.gameTick = function() {
       }
       dom.gamelogicAvg.innerHTML = Math.round(gamelogicTime[0]);
       dom.gamelogicHigh.innerHTML = Math.round(gamelogicTime[1]);
-      dom.gameLogicLow.innerHTML = Math.round(gamelogicTime[2]);
-      dom.gameLogicPeak.innerHTML = gamelogicTime[3];
+      dom.gamelogicLow.innerHTML = Math.round(gamelogicTime[2]);
+      dom.gamelogicPeak.innerHTML = gamelogicTime[3];
     }
   } else if (findingPlayers) {
     findPlayers();
@@ -1288,7 +1288,6 @@ window.renderTick = function() {
           if (diff < renderTime[2]) {
             renderTime[2] = diff;
           }
-
           dom.renderAvg.innerHTML = Math.round(renderTime[0]);
           dom.renderHigh.innerHTML = Math.round(renderTime[1]);
           dom.renderLow.innerHTML = Math.round(renderTime[2]);
