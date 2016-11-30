03934dbe08226e804a69566c8eb38b6ede0b7da4
Marlon Mckinnie <marlonmckinnie@gmail.com>
2016-11-30 00:18:41 -0500

    basic server

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('src'));
+app.use(express.static('./dist'));
 
 app.get('/', function(req, res) {
-  res.redirect('/meleelight.html');
+  res.redirect('/meleelightikd.html');
 });
 
 app.listen(3000, function() {
