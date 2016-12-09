7a1461e9838470bfe52b00ec5453f177fa68c863
sheaf <sam.derbyshire@gmail.com>
2016-12-09 17:10:26 +0100

    update final destination

--- src/stages/vs-stages/fdest.js
+++ src/stages/vs-stages/fdest.js
@@ -5,9 +5,9 @@ export default {
   box: [new Box2D([-85.6, -10], [85.6, 0])],
   platform: [],
   ground: [[new Vec2D(-85.6, 0), new Vec2D(85.6, 0)]],
-  ceiling: [ [new Vec2D(-50, -55), new Vec2D(-45, -56)], [new Vec2D(-45, -56), new Vec2D(45, -56)], [new Vec2D(45, -56), new Vec2D(50, -55)]],  
-  wallL: [[new Vec2D(-85.6, 0), new Vec2D(-85.6, -10)], [new Vec2D(-85.6, -10), new Vec2D(-65, -20)], [new Vec2D(-65, -20), new Vec2D(-65, -30)], [new Vec2D(-65, -30), new Vec2D(-55, -47)], [new Vec2D(-55, -47), new Vec2D(-50, -55)] ],
-  wallR: [[new Vec2D(85.6, 0), new Vec2D(85.6, -10)], [new Vec2D(85.6, -10), new Vec2D(65, -20)], [new Vec2D(65, -20), new Vec2D(65, -30)], [new Vec2D(65, -30), new Vec2D(55, -47)], [new Vec2D(55, -47), new Vec2D(50, -55)] ],
+  ceiling: [[new Vec2D(-50, -55), new Vec2D(-45, -56)], [new Vec2D(-45, -56), new Vec2D(45, -56)], [new Vec2D(45, -56), new Vec2D(50, -55)]],  
+  wallL: [[new Vec2D(-85.6, 0), new Vec2D(-85.6, -10)],[new Vec2D(-85.6, -10), new Vec2D(-65, -20)],[new Vec2D(-65, -20), new Vec2D(-65, -30)], [new Vec2D(-65, -30), new Vec2D(-60, -47)],[new Vec2D(-60, -47), new Vec2D(-50, -55)] ],
+  wallR: [[new Vec2D(85.6, 0), new Vec2D(85.6, -10)],[new Vec2D(85.6, -10), new Vec2D(65, -20)],[new Vec2D(65, -20), new Vec2D(65, -30)], [new Vec2D(65, -30), new Vec2D(60, -47)],[new Vec2D(60, -47), new Vec2D(50, -55)] ],
   startingPoint: [new Vec2D(-60, 10), new Vec2D(60, 10), new Vec2D(-20, 10), new Vec2D(20, 10)],
   startingFace: [1, -1, 1, -1],
   respawnPoints: [new Vec2D(16, 45), new Vec2D(-16, 45), new Vec2D(50, 45), new Vec2D(-50, 45)],
@@ -16,7 +16,7 @@ export default {
   ledge: [[0, 0], [0, 1]],
   ledgePos: [new Vec2D(-68.4, 0), new Vec2D(68.4, 0)],
   scale: 4.5,
-  offset: [600, 480],
+  offset: [600, 400],
   movingPlat: -1,
   movingPlatforms: function () {
   }
