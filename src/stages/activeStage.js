5d6295b8a20eda88c71df5661406a83bd2599cf0
sheaf <sam.derbyshire@gmail.com>
2016-12-10 11:54:18 +0100

    fix not being able to test stage from stage builder

--- src/stages/activeStage.js
+++ src/stages/activeStage.js
@@ -49,6 +49,10 @@ export let activeStage = {
   offset: [600, 480],
 };
 
+export function setActiveStageBuilderTestStage( stageTemp ) {
+  activeStage = stageTemp;
+}
+
 export function getActiveStage() {
   return activeStage;
 }
