aa43a16e914a18ab198984a703dbd6bbefce7c43
sheaf <sam.derbyshire@gmail.com>
2016-12-11 23:50:46 +0100

    update battlefield stage

--- src/stages/vs-stages/battlefield.js
+++ src/stages/vs-stages/battlefield.js
@@ -2,12 +2,12 @@ import {Box2D} from "../../main/util/Box2D";
 import {Vec2D} from "../../main/util/Vec2D";
 
 export default {
-  box: [new Box2D([-68.4, -9], [68.4, 0]), new Box2D([-29.3, -34], [-13.68, -9]), new Box2D([13.68, -34], [29.3, -9]), new Box2D([-13.68, -20], [13.68, -9])],
+  box: [new Box2D([-68.4, -9], [68.4, 0])],
   platform: [[new Vec2D(-57.6, 27.2), new Vec2D(-20, 27.2)], [new Vec2D(20, 27.2), new Vec2D(57.6, 27.2)], [new Vec2D(-18.8, 54.4), new Vec2D(18.8, 54.4)]],
   ground: [[new Vec2D(-68.4, 0), new Vec2D(68.4, 0)]],
-  ceiling: [[new Vec2D(-68.4, -9), new Vec2D(-29.3, -9)], [new Vec2D(-29.3, -34), new Vec2D(-13.68, -34)], [new Vec2D(13.68, -34), new Vec2D(29.3, -34)], [new Vec2D(29.3, -9), new Vec2D(68.4, -9)], [new Vec2D(-13.68, -20), new Vec2D(13.68, -20)]],
-  wallL: [[new Vec2D(-68.4, 0), new Vec2D(-68.4, -9)], [new Vec2D(-29.3, -9), new Vec2D(-29.3, -34)], [new Vec2D(13.68, -20), new Vec2D(13.68, -34)]],
-  wallR: [[new Vec2D(68.4, 0), new Vec2D(68.4, -9)], [new Vec2D(29.3, -9), new Vec2D(29.3, -34)], [new Vec2D(-13.68, -20), new Vec2D(-13.68, -34)]],
+  ceiling: [[ new Vec2D (-65, -6), new Vec2D (-36, -19)], [ new Vec2D (-29, -35), new Vec2D (-10, -40)], [new Vec2D (-10, -30), new Vec2D (10, -30)], [ new Vec2D (65, -6), new Vec2D (36, -19)], [ new Vec2D (29, -35), new Vec2D (10, -40)]],
+  wallL: [[ new Vec2D( -68.4, 0), new Vec2D (-65, -6) ], [ new Vec2D (-36, -19), new Vec2D (-39, -21 )  ], [ new Vec2D (-39, -21 ), new Vec2D (-33, -25)  ], [ new Vec2D (-33, -25), new Vec2D (-30, -29)  ], [ new Vec2D (-30, -29), new Vec2D (-29, -35)  ], [ new Vec2D (10, -30), new Vec2D (10, -40) ]],
+  wallR: [[ new Vec2D( 68.4, 0), new Vec2D (65, -6) ], [ new Vec2D (36, -19), new Vec2D (39, -21 )  ], [ new Vec2D (39, -21 ), new Vec2D (33, -25)  ], [ new Vec2D (33, -25), new Vec2D (30, -29)  ], [ new Vec2D (30, -29), new Vec2D (29, -35)  ], [ new Vec2D (-10, -30), new Vec2D (-10, -40)  ]],
   startingPoint: [new Vec2D(-50, 50), new Vec2D(50, 50), new Vec2D(-25, 5), new Vec2D(25, 5)],
   startingFace: [1, -1, 1, -1],
   respawnPoints: [new Vec2D(-50, 50), new Vec2D(50, 50), new Vec2D(-25, 35), new Vec2D(25, 35)],
@@ -17,7 +17,10 @@ export default {
   ledgePos: [new Vec2D(-68.4, 0), new Vec2D(68.4, 0)],
   scale: 4.5,
   offset: [600, 480],
-  movingPlat: -1,
+  connected: [[false, [["l",4], ["l",3], ["l",2], ["l",1]]], [false, [["r",1], ["r",2], ["r",3], ["r",4]]]],
   movingPlatforms: function () {
   }
 };
+
+
+
