f2c68bf8e7df2d7c4eb067430524513ba33d864f
Marlon Mckinnie <marlon.mckinnie@gmail.com>
2016-12-18 19:58:27 -0500

    server changes

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 var express = require('express');
 var app = express();
-
+var ExpressPeerServer = require('peer').ExpressPeerServer;
 app.use(express.static('./dist'));
 
 app.get('/', function(req, res) {
   res.redirect('/meleelight.html');
 });
+var server = app.listen(80);
 
-app.listen(9000, function() {
+var options = {
+  debug: true
+};
+
+//var server = require('http').createServer(app);
+
+app.use('/peerjs', ExpressPeerServer(server, options));
+
+server.listen(9000);
+
+
+var connected = [];
+server.on('connection', function (id) {
+  var idx = connected.indexOf(id); // only add id if it's not in the list yet
+  if (idx === -1) {console.log("peer connected"+ id);connected.push(id);}
+});
+server.on('disconnect', function (id) {
+  var idx = connected.indexOf(id); // only attempt to remove id if it's in the list
+  if (idx !== -1) {console.log("peer disconnected" + id);connected.splice(idx, 1);}
+});
+
+app.get('/connected-people', function (req, res) {
+  return res.json(connected);
 });
