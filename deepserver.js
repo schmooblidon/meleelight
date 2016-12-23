2067dea1c5b4dffe0b8a0529825bc6a778843167
Marlon Mckinnie <marlonmckinnie@gmail.com>
2016-12-23 08:33:47 -0500

    deployment stuff

--- deepserver.js
+++ deepserver.js
@@ -1,3 +1,18 @@
+var express = require('express');
+var app = express();
+app.use(express.static('./dist'));
+
+app.get('/', function(req, res) {
+  res.redirect('/meleelight.html');
+});
+
+app.set('port', (process.env.PORT || 5000));
+
+const appServer = app.listen((process.env.PORT || 5000));
+
+
+
+
 const DeepstreamServer = require('deepstream.io')
 const C = DeepstreamServer.constants;
 /*
