cb527384d8027b8c4cd77d70146e98c6de5bf3d0
Marlon Mckinnie <marlonmckinnie@gmail.com>
2016-12-23 20:32:37 -0500

    sticking to client on this branch

--- deepserver.js
+++ deepserver.js
@@ -1,28 +1,13 @@
+var express = require('express');
+var app = express();
+app.use(express.static('./dist'));
 
-const DeepstreamServer = require('deepstream.io');
-const C = DeepstreamServer.constants;
-/*
- The server can take
- 1) a configuration file path
- 2) null to explicitly use defaults to be overriden by server.set()
- 3) left empty to load the base configuration from the config file located within the conf directory.
- 4) pass some options, missing options will be merged with the base configuration
- */
-const server = new DeepstreamServer({
-  host: 'localhost',
-  port: process.env.PORT
+app.get('/', function(req, res) {
+  res.redirect('/meleelight.html');
 });
-server.set('logLevel', 'ERROR');
-console.log("using port "+process.env.PORT);
-/*
- Optionally you can specify settings to set in complex objects, such as dataTransforms, a HTTPServer
- or a plugin which you want to reuse within your application
- */
-// server.set('dataTransforms', [{
-//   topic: C.TOPIC.RPC,
-//   action: C.ACTIONS.REQUEST,
-//   transform: function(data, metaData) {}
-// }])
 
-// start the server
-server.start();
+app.set('port', (process.env.PORT || 5000));
+
+const appServer = app.listen((process.env.PORT || 5000));
+
+
