d97a8172bd6acab78e2b5ce5f1e4308e6f1217b7
Marlon Mckinnie <marlonmckinnie@gmail.com>
2016-12-25 23:33:17 -0500

    forgot the meat

---
+++ src/main/multiplayer/spectatorclient.js
@@ -0,0 +1,164 @@
+/*eslint-disable*/
+import $ from 'jquery';
+import {nullInputs, nullInput} from "../input";
+import deepstream from 'deepstream.io-client-js';
+import {
+  setPlayerType,
+  ports,
+  addPlayer,
+  currentPlayers,
+  mType,
+  setMtype,
+  setCurrentPlayer, player
+  , setCS
+  , changeGamemode
+  , setStageSelect
+  , startGame
+} from "../main";
+import {deepCopyObject} from "../util/deepCopyObject";
+import {setTokenPos, setChosenChar} from "../../menus/css";
+let ds = null;
+let peerId = null;
+let connectionReady = false;
+let GAME_ID;
+let playerID;
+let HOST_GAME_ID = null;
+
+export function logIntoServerAsSpectator() {
+  ds = deepstream("wss://deepml.herokuapp.com:443").login(null, onSpectate);
+
+}
+
+function startRoom() {
+  GAME_ID = ds.getUid().replace("-", "");
+  playerID = ds.getUid().replace("-", "");
+
+  ds.on('connectionStateChanged', function (connectionState) {
+    var cssClass;
+
+    if (connectionState === 'ERROR' || connectionState === 'CLOSED') {
+      cssClass = 'red';
+    }
+    else if (connectionState === 'OPEN') {
+      cssClass = 'green';
+    }
+    else {
+      cssClass = 'yellow';
+    }
+//apply this to the front end at some point
+    console.log("connection status : " + cssClass);
+  });
+
+
+}
+
+
+function onSpectate() {
+  connectionReady = true;
+  startRoom();
+
+  $("#spectate").on('click', (e) => {
+    var destId = prompt("Hosts's peer ID:");
+    connectToUser(destId);
+  });
+}
+
+let hostRoom = null;
+
+const connectedPeers = {};
+const peerConnections = {};
+const playerStatusRecords = {};
+const playerInputBuffer = [nullInputs(), nullInputs(), nullInputs(), nullInputs()];
+
+
+
+
+export function saveNetworkInputs(playerSlot, inputBuffer) {
+  playerInputBuffer[playerSlot][0] = inputBuffer;
+
+}
+
+
+export function connectAsSpectator() {
+
+  logIntoServerAsSpectator();
+
+
+}
+
+
+function syncSpectator(exactportnumber) {
+
+  //add host players
+  for (let i = 0; i < exactportnumber; i++) {
+    setPlayerType(i, 2);
+    setMtype(i, 99);
+    setCurrentPlayer(i, i);
+  }
+
+}
+
+
+function connect(record, name) {
+  // Handle a join connection.
+
+  ds.record.getRecord(name+'totalPlayers').whenReady(totalPlayerRecord=>{
+    syncSpectator(totalPlayerRecord.get().totalPlayers);
+  });
+
+  ds.event.subscribe(name + 'totalPlayers', data => {
+syncSpectator(data.totalPlayers);
+  });
+  ds.event.subscribe(name + 'player/', data => {
+    //  console.log("listener player  "+ GAME_ID);
+    //  console.log(data);
+    if (data) {
+      if (data.playerID !== playerID) {
+        if (data.inputBuffer && (data.playerSlot !== undefined)) {
+          saveNetworkInputs(data.playerSlot, data.inputBuffer);
+          player[data.playerSlot] = deepCopyObject(true, player[data.playerSlot], data.playerInfo);
+        }
+      }
+    }
+  });
+  ds.event.subscribe(name + 'charSelection/', data => {
+    if (data) {
+      setChosenChar(data.playerSlot, data.charSelected);
+    }
+  });
+  ds.event.subscribe(name + 'gameMode/', data => {
+    if (data) {
+      changeGamemode(data.gameMode);
+
+    }
+  });
+
+  ds.event.subscribe(name + 'startGame/', data => {
+    if (data) {
+      setStageSelect(data.stageSelected);
+      startGame();
+    }
+  });
+  peerConnections[name] = record;
+
+}
+
+
+function connectToUser(userName) {
+  const requestedPeer = userName;
+  if (!connectedPeers[requestedPeer]) {
+    HOST_GAME_ID = requestedPeer;
+    let playerRecord = ds.record.getRecord(requestedPeer + '-game').whenReady(statusRecord => {
+      connect(statusRecord, requestedPeer);
+
+    });
+
+
+    peerConnections[requestedPeer] = playerRecord;
+
+  }
+  connectedPeers[requestedPeer] = 1;
+
+}
+
+
