77b761f69c58be40ae4385108b58d222c3e4923d
sheaf <sam.derbyshire@gmail.com>
2016-12-26 03:03:45 +0100

    Logitech Dual Action controller support

--- src/main/input.js
+++ src/main/input.js
@@ -402,10 +402,11 @@ const rockx360Map    = [ 0, 1, 2, 3, 5, 4 , 4 , 7, 12, 15, 13, 14,  0,   1,   3,
 const wiiULinuxMap   = [ 0, 3, 1, 2, 6, 5 , 4 , 7, 8 , 11, 9 , 10,  0,   1,   3,   4,  2,  5  ]; // ID 11, Official Wii U GC Adapter using wii-u-gc-adapter on Linux
 const tigerChromeMap = [ 0, 1, 2, 3, 6, 5 , 4 , 7, 11, 9 , 10, 8 ,  0,   1,   2,   5,  7,  6  ]; // ID 12, TigerGame 3-in-1 adapter on chrome
 const betopMap       = [ 1, 2, 0, 3, 5, 7 , 6 , 9, 12, 13, 14, 15,  0,   1,   2,   5,  7,  6  ]; // ID 13, TigerGame 3-in-1 adapter on chrome (d-pad are axes not buttons, axes to be confirmed)
+const dualActionMap  = [ 0, 2, 3, 1, 5, 7 , 6 , 8, 12, 15, 13, 14,  0,   1,   2,   3,  5,  4  ]; // ID 14, Logitech Dual Action
                     //   a  b  x  y  z  r   l   s  du  dr  dd  dl  lsX  lsY  csX  csY  lA  rA
 
-export const controllerMaps = [ mayflashMap, vJoyMap     , raphnetV2_9Map, xbox360Map, tigergameMap, retrolinkMap, raphnetV3_2Map, brookMap, ps4Map, rockx360Map
-                              , null       , wiiULinuxMap, tigerChromeMap, betopMap   ];
+export const controllerMaps = [ mayflashMap, vJoyMap     , raphnetV2_9Map, xbox360Map, tigergameMap , retrolinkMap, raphnetV3_2Map, brookMap, ps4Map, rockx360Map
+                              , null       , wiiULinuxMap, tigerChromeMap, betopMap  , dualActionMap  ];
 
 // Checking gamepad inputs are well defined
 export function gpdaxis ( gpd, gpdID, ax ) { // gpd.axes[n] but checking axis index is in range
@@ -537,6 +538,11 @@ controllerIDMap.set("TigerGame", 12); // ID: TigerGame XBOX+PS2+GC Game Controll
 controllerIDMap.set("Betop Controller", 13);
 controllerIDMap.set("20bc-1264", 13);
 
+// ID 14, Logitech Dual Action controller
+controllerIDMap.set("Logitech Dual Action", 14);
+controllerIDMap.set("046d-c216", 14);
+controllerIDMap.set("46d-c216", 14);
+
 
 //--END OF CONTROLLER IDs-------------------------------------
     
@@ -580,6 +586,9 @@ export function controllerNameFromIDnumber(number) {
     case 13:
       return "Betop controller";
       break;
+    case 14:
+      return "Logitech Dual Action controller";
+      break;
     default:
       return "error: controller detected but not supported";
   }
@@ -606,6 +615,7 @@ function axisDataFromIDNumber(number) {
     case 3: // XInput controllers
     case 8: // PS4 controller    
     case 9: // Rock Candy Xbox 360 controller
+    case 14: // Logitech Dual Action controller
       return ( fromCardinals ( [0, 0], -1, 1, 1, -1) );
       break;
     default:
