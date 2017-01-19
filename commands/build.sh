a0f0764f3fe84aecb1f71aed76d580fda0daf499
dandeto <danielmichaelmills@gmail.com>
2017-01-19 07:26:33 -0600

    Update build.sh

--- commands/build.sh
+++ commands/build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 cd -
 clear
-#!/bin/bash
-cd-
-clear
 echo "There are 2 build types - developer and production. Developer build updates changes, while production is the optimized build."
 echo "Would you like to build dev (a) or production (b) version: "
 read input_variable
