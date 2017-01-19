1b34c0f79c2ba42366655be705805c35ab15d7a6
dandeto <danielmichaelmills@gmail.com>
2017-01-19 07:20:16 -0600

    Fixed Broken Code

--- commands/install.sh
+++ commands/install.sh
@@ -1,29 +1,14 @@
 #!/bin/bash
-cd -
+cd-
 clear
-echo There are 2 installation methods. Installing packages with node package manager - npm - or with yarn. Since you have node.js you have npm, but you will only have yarn if you downloaded it seperatly.
-sleep 3s
+echo "There are 2 installation methods. Installing packages with node package manager(npm) or with yarn. Since you have node.js you have npm, but you will only have yarn if you downloaded it seperatly."
 
-start () {
-read -p "would you like to install using yarn(a) or npm install(b):" Input
+echo "Please choose (a) yarn or (b) npm: "
+read input_variable
+echo "You entered: ${input_variable}"
 
-myParam=`echo "$Input" | tr 'a-z' 'A-Z'`
-if ["A"=="$myParam"]; then
-	yarn
-if ["B"=="$myParam"]; then
-	npm
-else
-	start
-clear
-fi
-}
-
-yarn () {
-yarn init
+    case ${input_variable} in
+        a ) yarn init; break;;
+        b )  npm install;;
+    esac
 exit
-}
-
-npm () {
-npm install
-exit
-}
