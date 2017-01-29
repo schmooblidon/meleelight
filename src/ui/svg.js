aab2283857565354dd4fdd3ae3bc9af807ba6319
sheaf <sam.derbyshire@gmail.com>
2017-01-29 19:31:58 +0100

    incorporate transforming into SVG creation

--- src/ui/svg.js
+++ src/ui/svg.js
@@ -15,25 +15,35 @@ function getSVGTransform (transform : void | Transform) : string {
   let output = "";
   if ( transform !== undefined) {
     if (transform.kind === "translate") {
-      output = "transform=\x22translate("+transform.vector.x+","+transform.vector.y+")\x22";
+      output = "translate("+transform.vector.x+","+transform.vector.y+")";
     }
     else if (transform.kind === "scale") {
-      output = "transform=\x22scale("+transform.scale.x+","+transform.scale.y+")\x22";
+      output = "scale("+transform.scale.x+","+transform.scale.y+")";
     }
     else if (transform.kind === "rotate") {
       let center = "";
       if (transform.center !== undefined) {
         center = ","+transform.center.x+","+transform.center.y;
       }
-      output = "transform=\x22rotate("+transform.angle+""+center+")\x22";
+      output = "rotate("+transform.angle+""+center+")";
     }
     else if (transform.kind === "matrix") {
       let vector = ",0,0";
       if (transform.translate !== undefined) {
         vector = ","+transform.translate.x+","+transform.translate.y;
       }
-      output = "transform=\x22matrix("+transform.matrix[0][0]+","+transform.matrix[1][0]+","+transform.matrix[0][1]+","+transform.matrix[1][1]+""+vector+")\x22";
+      output = "matrix("+transform.matrix[0][0]+","+transform.matrix[1][0]+","+transform.matrix[0][1]+","+transform.matrix[1][1]+""+vector+")";
     }
+  }
+  return output;
+}
+
+function concatTransforms( transforms : void | Array<Transform>) : string {
+  let output = "";
+  if (transforms !== undefined && transforms.length > 0) {
+    output += "transform=\x22";
+    output += transforms.map(getSVGTransform).join(" ");
+    output += "\x22";
   }
   return output;
 }
@@ -65,22 +75,21 @@ function getSVGStyle ( style : Style ) : string {
   return "opacity:"+opacity+""+svgFillStyle+""+svgStrokeStyle+"";
 }
 
-function makeIntoSVGPath ( id : string, path : string, style : Style, offset : void | Vec2D) : string {
+function makeIntoSVGPath ( id : string, path : string, style : Style, transforms : void | Array<Transform>) : string {
   const svgStyle = getSVGStyle(style);
-  let transform = "";
-  if (offset !== undefined) {
-    transform = " transform=\x22translate("+offset.x+","+offset.y+")\x22";
-  }
-  return "<path id=\x22"+id+"\x22 style=\x22"+svgStyle+"\x22 d=\x22"+path+"\x22"+transform+"/>";
+  const transform = concatTransforms(transforms);
+  return "<path id=\x22"+id+"\x22 style=\x22"+svgStyle+"\x22 d=\x22"+path+"\x22 "+transform+"/>";
 }
 
-function makeIntoSVGCircle ( id : string, circle : Circle, style : Style ) : string {
+function makeIntoSVGCircle ( id : string, circle : Circle, style : Style, transforms : void | Array<Transform> ) : string {
   const svgStyle = getSVGStyle(style);
-  return "<circle id=\x22"+id+"\x22 style=\x22"+svgStyle+"\x22 cx=\x22"+circle.center.x+"\x22 cy=\x22"+circle.center.y+"\x22 r=\x22"+circle.radius+"\x22 />";
+  const transform = concatTransforms(transforms);
+  return "<circle id=\x22"+id+"\x22 style=\x22"+svgStyle+"\x22 cx=\x22"+circle.center.x+"\x22 cy=\x22"+circle.center.y+"\x22 r=\x22"+circle.radius+"\x22 "+transform+" />";
 }
 
-function makeIntoSVGRect ( id : string, rect : Rect, style : Style ) : string {
+function makeIntoSVGRect ( id : string, rect : Rect, style : Style, transforms : void | Array<Transform> ) : string {
   const svgStyle = getSVGStyle(style);
+  const transform = concatTransforms(transforms);
   let topLeft = "";
   if (rect.topLeft !== undefined) {
     topLeft = "x=\x22"+rect.topLeft.x+"\x22 y=\x22"+rect.topLeft.y+"\x22";
@@ -89,7 +98,7 @@ function makeIntoSVGRect ( id : string, rect : Rect, style : Style ) : string {
   if (rect.rounded !== undefined) {
     rounded = "rx=\x22"+rect.rounded.x+"\x22 ry=\x22"+rect.rounded.y+"\x22";
   }
-  return "<rect id=\x22"+id+"\x22 style=\x22"+svgStyle+"\x22 width=\x22"+rect.width+"\x22 height=\x22"+rect.height+""+topLeft+""+rounded+" />";
+  return "<rect id=\x22"+id+"\x22 style=\x22"+svgStyle+"\x22 width=\x22"+rect.width+"\x22 height=\x22"+rect.height+""+topLeft+""+rounded+" "+transform+" />";
 }
 
 
