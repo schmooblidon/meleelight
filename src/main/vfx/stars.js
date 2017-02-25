3f1d366e953e2efea6717dc9560a54d4c510a773
Marlon Mckinnie <marlonmckinnie@gmail.com>
2017-02-24 22:15:17 -0500

    added star data back in

--- src/main/vfx/stars.js
+++ src/main/vfx/stars.js
@@ -8,7 +8,9 @@ export function stars(tX, tY, n, minSpread, maxSpread) {
   
   for (let i = 0; i < n; i++) {
     const [deltaX, deltaY] = randomAnnulusPoint(0, 0, minSpread, maxSpread);
-    addToVfxQueue({ name : "star", timer : 0, pos : new Vec2D(tX,tY), face : [deltaX, deltaY], facing : 0.4 + 0.8*Math.random()});
+    const instance = {};
+    Object.assign(instance,vfx["star"],{ name : "star", timer : 0, pos : new Vec2D(tX,tY), face : [deltaX, deltaY], facing : 0.4 + 0.8*Math.random()});
+    addToVfxQueue(instance);
   }
  
 }
