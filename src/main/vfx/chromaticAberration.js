8a100a2defcf14659a7a2a3ffa9e8aad138bfdae
sheaf <sam.derbyshire@gmail.com>
2017-02-27 20:16:51 +0100

    avoid save/restore for chromatic aberration rendering

--- src/main/vfx/chromaticAberration.js
+++ src/main/vfx/chromaticAberration.js
@@ -2,8 +2,6 @@ import {makeColour} from "main/vfx/makeColour";
 
 export function chromaticAberration( ctx, drawFunction, col1, col2, opacity, vec ) {
 
-  ctx.save();
-
   ctx.globalCompositeOperation = "screen";
 
   drawFunction(makeColour(0, col1.g, 0, opacity), makeColour(0, col2.g, 0, opacity));
@@ -14,6 +12,8 @@ export function chromaticAberration( ctx, drawFunction, col1, col2, opacity, vec
   ctx.translate(2*vec.x,2*vec.y);
   drawFunction(makeColour(0, 0, col1.b, opacity), makeColour(0, 0, col2.b, opacity));
 
-  ctx.restore();
+  // reset canvas state
+  ctx.translate(-vec.x,-vec.y);
+  ctx.globalCompositeOperation = "source-over";
   
 }
