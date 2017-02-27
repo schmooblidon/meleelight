25d3fbdcbe8c277cbb6be94028636ae2fc96afbb
sheaf <sam.derbyshire@gmail.com>
2017-02-27 06:08:40 +0100

    flying tetrahedra

---
+++ src/stages/3D.js
@@ -0,0 +1,33 @@
+
+import {Vec2D} from "main/util/Vec2D";
+
+export class Vec3D { x : number; y : number; z : number;
+  constructor( x : number, y : number, z : number ) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  };
+};
+
+function dot (v1, v2) {
+  return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+
+// this function computes the projected coordinates on a 2D-screen of a point in 3D,
+// as seen from the point O, with direction vector D, and screen with center at C = O + D
+// on this screen, "right" corresponds to the vector R, and "up" to U
+// note that U and R should be orthogonal to D and to each-other
+export function splatCoordinates ( o, d, r, u, p) {
+  const op = new Vec3D ( p.x - o.x, p.y - o.y, p.z - o.z);
+  const t = dot (d, d) / dot (op, d);
+  if (t < 0) {
+    return null;
+  }
+  const s = new Vec3D ( (1-t) * o.x + t * p.x
+                      , (1-t) * o.y + t * p.y
+                      , (1-t) * o.z + t * p.z );
+  const c = new Vec3D ( o.x + d.x, o.y + d.y, o.z + d.z);
+  const x = r.x * (s.x - c.x) + r.y * (s.y - c.y) + r.z * (s.z - c.z);
+  const y = u.x * (s.x - c.x) + u.y * (s.y - c.y) + u.z * (s.z - c.z);
+  return new Vec2D( x, y );
+}
--- src/stages/synthwave.js
+++ src/stages/synthwave.js
@@ -1,15 +1,27 @@
-import {bg1,bg2} from "main/main";
+import {bg1,bg2,fg1,fg2} from "main/main";
 import {Vec2D} from "main/util/Vec2D";
+import {Vec3D, splatCoordinates} from "./3D";
 import {chromaticAberration} from "main/vfx/chromaticAberration";
 import {makeColour} from "main/vfx/makeColour";
 
 const lines = [];
 const hScale = 1;
-const lineCount = 40;
-const height = 400;
-const heightOffset = 100;
-const speed = -0.03;
+const lineCount = 20;
+const height = 1000;
+const heightOffset = -500;
+const speed = -0.04;
 const focal = 1;
+
+// camera data
+const origin = new Vec3D(0,3,-5);
+const direction = new Vec3D(0,0,1);
+const right = new Vec3D(1,0,0);
+const up = new Vec3D(0,1,0);
+
+const wireframeEdges = [[0,1], [0,2], [1,2],[0,3],[1,3],[2,3]];
+const wireframeVertices = [ new Vec3D(-3,0,10), new Vec3D(-4.5, 0, 10.866), new Vec3D(-4.5,0,9.134), new Vec3D(-4,0.866,10) ];
+let wireframeZMin = 9.134;
+let wireframeZMax = 10.866;
 
 const offset = - height + height * lineCount * hScale / (focal + lineCount * hScale);
 
@@ -19,12 +31,6 @@ for (let i=0;i<lineCount;i++) {
 }
 
 export function drawSynthWaveInit() {
-  const bgGrad = bg1.createLinearGradient(0, 0, 1200, 0);
-  bgGrad.addColorStop(0, "#482842");
-  bgGrad.addColorStop(0.5, "#000000");
-  bgGrad.addColorStop(1, "#261452");
-  bg1.fillStyle = bgGrad;
-  bg1.fillRect(0,0,1200,750);
   // sun
   // create gradient
   const top = 150;
@@ -32,14 +38,15 @@ export function drawSynthWaveInit() {
   const radius = 200;
   const bottom = 500;
   const sunGlow = bg1.createRadialGradient(600,top+radius,radius,600,top+radius,radius+60);
-  sunGlow.addColorStop(0, "rgba(200, 62, 229, 0.3)");
-  //sunGlow.addColorStop(0.2, "rgba(200, 62, 229, 0.3)");
-  sunGlow.addColorStop(1, "rgba(200, 62, 229, 0)");
+  sunGlow.addColorStop(0, "rgba(232, 76, 110, 0.4)");
+  sunGlow.addColorStop(1, "rgba(142, 67, 168, 0)");
   bg1.fillStyle = sunGlow;
   bg1.fillRect(0,0,1200,750);
   const sunGrad = bg1.createLinearGradient(0, top, 0, top+radius*2);
-  sunGrad.addColorStop(0, "#efe03a");
-  sunGrad.addColorStop(1, "#ee6add");
+  sunGrad.addColorStop(0, "rgba(250, 227, 59, 1)");
+  sunGrad.addColorStop(0.5, "rgba(250, 97, 96, 1)");
+  sunGrad.addColorStop(0.75, "rgba(250, 52, 104, 1)");
+  sunGrad.addColorStop(1, "rgba(250, 60, 134, 1)");
   bg1.fillStyle = sunGrad;
   // creating clipping path
   const lineSeperation = 40;
@@ -63,17 +70,25 @@ export function drawSynthWaveInit() {
   bg1.closePath();
   bg1.fill();
   bg1.restore();
+  // draw screen over everything else
+  fg1.globalCompositeOperation = "screen";
+  const bgGrad = fg1.createLinearGradient(0, 0, 0, 750);
+  bgGrad.addColorStop(0, "rgba(253, 117, 121, 0.6");
+  bgGrad.addColorStop(0.6, "rgba(224, 36, 131, 0.6");
+  fg1.fillStyle = bgGrad;
+  fg1.fillRect(0,0,1200,750);
+  fg1.globalCompositeOperation = "source-over";
 }
 
 export function drawSynthWave() {
-  const col = { r : 130, g : 30, b : 150 };
+  const col = { r : 130, g : 30, b : 190 };
   // draw vertical lines
   chromaticAberration( bg2, (c1,c2) => drawVertLines(c1), col, col, 1, new Vec2D(1.3,0) );
   // draw horizontal lines
   chromaticAberration( bg2, (c1,c2) => drawHorizLines(c1), col, col, 1, new Vec2D(0,1.3) );
   // thick line on the horizon, at y-coordinate height + heightOffset;
   bg2.lineWidth = 5;
-  bg2.strokeStyle = "#c238d4";
+  bg2.strokeStyle = makeColour(col.r, col.g, col.b, 1);
   const y = height + heightOffset;
   bg2.beginPath();
   bg2.moveTo(0,y);
@@ -82,6 +97,21 @@ export function drawSynthWave() {
   bg2.lineWidth = 2;
   bg2.strokeStyle = "#dc6eec";
   bg2.stroke();
+
+  fg2.lineWidth = 3;
+  fg2.strokeStyle = makeColour(230, 120, 20, 1);
+  fg2.beginPath();
+  for (let k=0; k<wireframeEdges.length; k++) {
+    const v1 = wireframeVertices[wireframeEdges[k][0]];
+    const v2 = wireframeVertices[wireframeEdges[k][1]];
+    const p1 = splatCoordinates(origin, direction, right, up, v1);
+    const p2 = splatCoordinates(origin, direction, right, up, v2);
+    if (p1 !== null && p2 !== null) {
+      fg2.moveTo(600 + 166*p1.x, 300 - 166*p1.y);
+      fg2.lineTo(600 + 166*p2.x, 300 - 166*p2.y);
+    }
+    fg2.stroke();
+  }
   // move lines for the next frame
   for (let i=0;i<lineCount;i++) {
     lines[i] += speed;
@@ -92,6 +122,30 @@ export function drawSynthWave() {
       lines[i] = 0;
     }
   }
+  // move wireframe
+  if (wireframeZMax < -6) {
+    wireframeZMin += 20;
+    wireframeZMax += 20;
+    for (let i=0; i<wireframeVertices.length; i++) { 
+      wireframeVertices[i].z += 20;
+    }
+  }
+  else if (wireframeZMin > 80) {
+    wireframeZMin -= 86;
+    wireframeZMax -= 86;
+    for (let i=0; i<wireframeVertices.length; i++) { 
+      wireframeVertices[i].z -= 86;
+    }
+  }
+  else {
+    wireframeZMin += speed;
+    wireframeZMax += speed;
+    for (let i=0; i<wireframeVertices.length; i++) { 
+      wireframeVertices[i].z += speed;
+    }
+  }
+
+
 }
 
 function drawVertLines(col) {
