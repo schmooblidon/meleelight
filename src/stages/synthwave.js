b0e1de069343f62ee12f9d393fbbf7c3c637314e
sheaf <sam.derbyshire@gmail.com>
2017-02-27 23:02:46 +0100

    accurate perspective transformations and visual consistency

--- src/stages/synthwave.js
+++ src/stages/synthwave.js
@@ -4,21 +4,28 @@ import {Vec3D, splatCoordinates, add} from "./3D";
 import {chromaticAberration} from "main/vfx/chromaticAberration";
 import {makeColour} from "main/vfx/makeColour";
 
-const lines = [];
-const hScale = 1;
-const lineCount = 20;
-const height = 1000;
-const heightOffset = -500;
+const hScale = 0.8;
+const hLineCount = 20;
+const vScale = 2.5;
+const vLineCount = 15; // number of vertical lines is actually 2*vLineCount+1
+const height = 3;
+const heightOffset = 497;
 const speed = -0.04;
-const focal = 1;
+const focal = 5;
 
 // camera data
-const origin = new Vec3D(0,3,-5);
+const origin = new Vec3D(0,height,-focal);
 const direction = new Vec3D(0,0,1);
 const right = new Vec3D(1,0,0);
 const up = new Vec3D(0,1,0);
 
-const offset = - height + height * lineCount * hScale / (focal + lineCount * hScale);
+const furthest = hLineCount*hScale-focal;
+
+const offsetPoint1 = splatCoordinates(origin, direction, right, up, new Vec3D(0,0, 1e5));
+const offsetPoint2 = splatCoordinates(origin, direction, right, up, new Vec3D(0,0, furthest));
+const offset = offsetPoint1.y - offsetPoint2.y;
+
+const horizonRightX = scaleToCanvas(splatCoordinates(origin, direction, right, up, new Vec3D(vScale*vLineCount,0, furthest))).x;
 
 function tetrahedronAt(v) {
   return { edges    : [[0,1], [0,2], [1,2],[0,3],[1,3],[2,3]]
@@ -28,13 +35,18 @@ function tetrahedronAt(v) {
          , zmax :  0.866 };
 };
 
-const objects = [ tetrahedronAt( new Vec3D(-4,0,15))
-                , tetrahedronAt( new Vec3D(-3,10,24))
-                , tetrahedronAt( new Vec3D(4,5,26))
-                , tetrahedronAt( new Vec3D(2,-3,37)) ];
+const objects = [ tetrahedronAt( new Vec3D(-6,0,15) )
+                , tetrahedronAt( new Vec3D(-4,0,24) )
+                , tetrahedronAt( new Vec3D(2 ,0,26) )
+                , tetrahedronAt( new Vec3D(7 ,0,37) ) ];
 
-for (let i=0;i<lineCount;i++) {
-  lines.push(i*hScale);
+// horizontal lines
+for (let i=0;i<hLineCount;i++) {
+  objects.push( { edges    : [[0,1]]
+                , vertices : [ new Vec3D(-15*vScale,0,0), new Vec3D(15*vScale,0,0)]
+                , offset   : new Vec3D(0,0,i*hScale-focal)
+                , zmin : 0
+                , zmax : 0 } );
 }
 
 export function drawSynthWaveInit() {
@@ -91,40 +103,22 @@ export function drawSynthWave() {
   const col = { r : 130, g : 30, b : 190 };
   // draw vertical lines
   chromaticAberration( bg2, (c1,c2) => drawVertLines(c1), col, col, 1, new Vec2D(1.3,0) );
-  // draw horizontal lines
-  chromaticAberration( bg2, (c1,c2) => drawHorizLines(c1), col, col, 1, new Vec2D(0,1.3) );
-  // thick line on the horizon, at y-coordinate height + heightOffset;
-  bg2.lineWidth = 5;
+  // horizon, at y-coordinate height + heightOffset;
+  bg2.lineWidth = 3;
   bg2.strokeStyle = makeColour(col.r, col.g, col.b, 1);
   const y = height + heightOffset;
   bg2.beginPath();
-  bg2.moveTo(0,y);
-  bg2.lineTo(1200,y);
-  bg2.stroke();
-  bg2.lineWidth = 2;
-  bg2.strokeStyle = "#dc6eec";
+  bg2.moveTo(1198-horizonRightX,y);
+  bg2.lineTo(   2+horizonRightX,y);
   bg2.stroke();
 
   drawObjects(bg2, col);
 
-  // move lines for the next frame
-  for (let i=0;i<lineCount;i++) {
-    lines[i] += speed;
-    if (lines[i] < 0) {
-      lines[i] = lineCount * hScale;
-    }
-    else if (lines[i] > lineCount * hScale) {
-      lines[i] = 0;
-    }
-  }
   // move objects
   for (let o=0; o<objects.length; o++) {
     const obj = objects[o];
-    if (obj.offset.z + obj.zmax < -6) {
-      obj.offset.z += 20;
-    }
-    else if (obj.offset.z + obj.zmin > 60) {
-      obj.offset.z -= 60;
+    if (obj.offset.z + obj.zmax < -focal) {
+      obj.offset.z = furthest - obj.zmin;
     }
     else {
       obj.offset.z += speed;
@@ -151,36 +145,38 @@ function drawObject(ctx, col, obj) {
     const p1 = splatCoordinates(origin, direction, right, up, v1);
     const p2 = splatCoordinates(origin, direction, right, up, v2);
     if (p1 !== null && p2 !== null) {
-      ctx.moveTo(600 + 166*p1.x, 300 - 166*p1.y);
-      ctx.lineTo(600 + 166*p2.x, 300 - 166*p2.y);
+      const sp1 = scaleToCanvas(p1);
+      const sp2 = scaleToCanvas(p2);
+      ctx.moveTo(sp1.x, sp1.y);
+      ctx.lineTo(sp2.x, sp2.y);
     }
   }
   ctx.stroke();
 }
 
+function scaleToCanvas( v ) {
+  return new Vec2D( 600+166*v.x
+                  , height+heightOffset-166*(offset+v.y) );
+}
+
 function drawVertLines(col) {
+  bg2.save();
+  bg2.beginPath();
+  bg2.moveTo(0,750);
+  bg2.lineTo(1200,750);
+  bg2.lineTo(1200, height + heightOffset);
+  bg2.lineTo(0, height + heightOffset);
+  bg2.clip();
+  const sp2 = scaleToCanvas(offsetPoint1);
   bg2.lineWidth = 3;
   bg2.strokeStyle = col;
   bg2.beginPath();
-  for (let i=-12;i<13;i++) {
-    bg2.moveTo(600+(1200/25)*i,height + heightOffset);
-    bg2.lineTo(600+(1200/7)*i,750);
+  for (let i=-vLineCount;i<vLineCount+1;i++) {
+    const p1 = splatCoordinates(origin, direction, right, up, new Vec3D(vScale*i,0,-focal+1));
+    const sp1 = scaleToCanvas(p1);
+    bg2.moveTo(sp1.x, sp1.y);
+    bg2.lineTo(sp2.x, sp2.y);
   }
   bg2.stroke();
-}
-
-function drawHorizLines(col) {
-  bg2.lineWidth = 3;
-  bg2.strokeStyle = col;
-  bg2.beginPath();
-  for (let i=0;i<lineCount;i++) {
-    const y = projectedYCoord(lines[i]) ;
-    bg2.moveTo(0   , y);
-    bg2.lineTo(1200, y);
-  }
-  bg2.stroke();
-}
-
-function projectedYCoord ( y ) {
-  return heightOffset + offset + 2 * height - height * y / (focal + y);
+  bg2.restore();
 }
